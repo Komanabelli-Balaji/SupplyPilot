5ef11d8cff4f11f4490b97dc452abe605001f08f
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-06-04 16:15:29 +0000

    Added fallback model

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,5 +3,11 @@ import os
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+api_key = os.getenv("GOOGLE_API_KEY")
+
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY is not set")
+
+os.environ["GOOGLE_API_KEY"] = api_key
 MODEL = os.getenv("MODEL")
+FALLBACK_MODEL = os.getenv("FALLBACK_MODEL")
