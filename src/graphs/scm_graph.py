142d2ef7cee18a1e0a7b4e959bc5a152604d92e8
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-07-31 14:37:09 +0000

    Restructured graph

--- src/graphs/scm_graph.py
+++ src/graphs/scm_graph.py
@@ -1,66 +1,31 @@
-from langgraph.graph import  StateGraph, START, END
+from langgraph.graph import END, START, StateGraph
 
-from state.scm_state import SCMState
 from negotiation.nodes import (
-    retailer_node,
-    distributor_node,
-    factory_node,
-    supervisor_node,
-    distributor_final_offer_node,
-    retailer_review_node,
-    distributor_revision_node
+    distributor_factory_node,
+    needs_factory,
+    retailer_distributor_node,
+    settlement_node,
 )
-
-def distributor_route(state):
-    if state["distributor_decision"].get("shortage", 0) > 0:
-        return "factory"
-
-    return "offer"
-
-def supervisor_route(state):
-    if state["final_decision"]["consensus"]:
-        return "done"
-
-    if (state["negotiation_round"] >= state["max_rounds"]):
-        return "done"
-
-    return "retry"
+from state.scm_state import SCMState
 
 builder = StateGraph(SCMState)
 
-builder.add_node("retailer", retailer_node)
-builder.add_node("distributor", distributor_node)
-builder.add_node("factory", factory_node)
-builder.add_node("distributor_offer", distributor_final_offer_node)
-builder.add_node("retailer_review", retailer_review_node)
-builder.add_node("distributor_revision", distributor_revision_node)
-builder.add_node("supervisor", supervisor_node)
+builder.add_node("retailer_distributor", retailer_distributor_node)
+builder.add_node("distributor_factory", distributor_factory_node)
+builder.add_node("settlement", settlement_node)
 
-builder.add_edge(START, "retailer")
-builder.add_edge("retailer", "distributor")
+builder.add_edge(START, "retailer_distributor")
 
 builder.add_conditional_edges(
-    "distributor",
-    distributor_route,
+    "retailer_distributor",
+    needs_factory,
     {
-        "factory": "factory",
-        "offer": "distributor_offer"
-    }
+        "factory": "distributor_factory",
+        "settlement": "settlement",
+    },
 )
 
-builder.add_edge("factory", "distributor_offer")
-builder.add_edge("distributor_offer", "retailer_review")
-builder.add_edge("retailer_review", "supervisor")
-
-builder.add_conditional_edges(
-    "supervisor",
-    supervisor_route,
-    {
-        "done": END,
-        "retry": "distributor_revision"
-    }
-)
-
-builder.add_edge("distributor_revision", "distributor_offer")
+builder.add_edge("distributor_factory", "settlement")
+builder.add_edge("settlement", END)
 
 graph = builder.compile()
