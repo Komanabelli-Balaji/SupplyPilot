d472a47fd4d4800ffd53dd25ff88176b6b78654c
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-08-02 12:23:40 +0000

    updated tools

--- src/agents/factory.py
+++ src/agents/factory.py
@@ -1,17 +1,23 @@
 from llms.provider import Provider
 from prompts.system_prompt import FACTORY_SYSTEM_PROMPT
 from schemas.negotiation import NegotiationProposal
-from tools.capacity_tools import get_overtime_capacity, get_regular_capacity
-from tools.economics_tools import get_factory_production_cost, get_factory_selling_price
+from tools.capacity_tools import (
+    get_overtime_capacity,
+    get_regular_capacity,
+)
+from tools.economics_tools import (
+    get_factory_economics,
+    get_factory_policy,
+)
 from tools.inventory_tools import get_inventory
 
 factory = Provider(
     tools=[
         get_inventory,
+        get_factory_policy,
+        get_factory_economics,
         get_regular_capacity,
         get_overtime_capacity,
-        get_factory_production_cost,
-        get_factory_selling_price,
     ],
     response_format=NegotiationProposal,
     system_prompt=FACTORY_SYSTEM_PROMPT
--- src/agents/retailer.py
+++ src/agents/retailer.py
@@ -1,26 +1,17 @@
 from llms.provider import Provider
 from prompts.system_prompt import RETAILER_SYSTEM_PROMPT
 from schemas.negotiation import NegotiationProposal
-from tools.demand_tools import get_current_demand, get_forecast_demand
-from tools.economics_tools import get_annual_demand, get_holding_cost, get_ordering_cost
-from tools.inventory_math import (
-    calculate_eoq,
-    calculate_reorder_point,
-    calculate_safety_stock,
+from tools.economics_tools import (
+    get_retailer_economics,
+    get_retailer_policy,
 )
 from tools.inventory_tools import get_inventory
 
 retailer = Provider(
     tools=[
         get_inventory,
-        get_current_demand,
-        get_forecast_demand,
-        get_ordering_cost,
-        get_holding_cost,
-        get_annual_demand,
-        calculate_eoq,
-        calculate_reorder_point,
-        calculate_safety_stock,
+        get_retailer_policy,
+        get_retailer_economics,
     ],
     response_format=NegotiationProposal,
     system_prompt=RETAILER_SYSTEM_PROMPT
